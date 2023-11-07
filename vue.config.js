4852b092c5cba3c7068af62d122094792c8ee4ba
sauricat <linshu1729@protonmail.com>
2023-11-07 06:47:32 -0500

    [F] Try fixing vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true
-    rules: [
-        // ... other rules omitted
-
-        // this will apply to both plain `.scss` files
-        // AND `<style lang="scss">` blocks in `.vue` files
-        {
-            test: /\.scss$/,
-            use: [
-                'vue-style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }
-    ]
 })
