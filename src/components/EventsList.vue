8763c58f08b507eaee33f1e2e2fb61576bb2c745
sauricat <linshu1729@protonmail.com>
2023-11-09 02:16:02 -0500

    [U] Separate events list module

---
+++ src/components/EventsList.vue
@@ -0,0 +1,50 @@
+<template>
+  <events>
+    <section>
+      <h2> Events</h2>
+      <event>
+        <img class="cover"
+             src="/events/2023TDOREvent.png"
+             alt="2023 TDoR Craft Event in Toronto">
+        <p> Join us on November 18, 2pm for our TDoR craft event. We welcome all participants to create a special gift, offering, or memento for their loved one(s). <br/> This event will be available in English, Mandarin Chinese, Wu Chinese, and Japanese. <br/> Location: 519 (519 Church St, Toronto) <br/> <a href="linktr.ee/oneamongus">Registration</a> is mandatory for this event. Please message us for any accommodation request. </p>
+      </event>
+    </section>
+
+    <section>
+      <h2> Past Events</h2>
+      <event>
+        <img class="cover"
+             src="/events/bhd-poster.jpg"
+             alt="Bright, Hope, Dream">
+        <p> Bright, Hope, Dream: TiA! Academic and Life Abroad Workshop <br/> First Talk: Canada and Nordic Countries @ 2023-10-28 14:00 UTC <br/> Join our <a href="#top">Social Media</a> for details. </p>
+      </event>
+      <event>
+        <img class="cover"
+             src="/events/lacan-reading.png"
+             alt="Lacan reading group">
+        <p> Reading Group: Lacanian Psychoanalysis for Activists <br/> Online from 2023-06-17 to 2023-08-10. </p>
+      </event>
+      <event>
+        <img class="cover"
+             src="/events/deleuze-reading.jpg"
+             alt="Deleuze reading group">
+        <p> Reading Group of <i> A Thousand Plateaus </i> <br/> Online from 2023-04-22. <br/> First series of seminars ended. Second in preparation. </p>
+      </event>
+      <event>
+        <img class="cover"
+             src="/events/miyazawa-poster.jpg"
+             alt="Poster of livestream reading of Night of the Milky Way Railway">
+        <p> Livestream reading of <i>Night of the Milky Way Railway</i> on TDOV 2023, by One Among Us & Project Trans. <br/> <a href="https://www.youtube.com/watch?v=TGhNTd-2I3c"> Replay </a> is available on YouTube. </p>
+      </event>
+    </section>
+  </events>
+</template>
+
+<style lang="scss">
+events {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row: 3;
+}
+
+</style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -8,53 +8,22 @@
 
     <p> We are hiring editors! Please contact <a href="mailto:tia@oneamongus.ca">tia@oneamongus.ca</a>.</p>
   </header>
-  <main>
-    <section>
-      <h2> Events</h2>
-      <event>
-	<img class="cover"
-             src="/events/2023TDOREvent.png"
-             alt="2023 TDoR Craft Event in Toronto">
-	<p> Join us on November 18, 2pm for our TDoR craft event. We welcome all participants to create a special gift, offering, or memento for their loved one(s). <br/> This event will be available in English, Mandarin Chinese, Wu Chinese, and Japanese. <br/> Location: 519 (519 Church St, Toronto) <br/> <a href="linktr.ee/oneamongus">Registration</a> is mandatory for this event. Please message us for any accommodation request. </p>
-      </event>
-    </section>
 
-    <section>
-      <h2> Past Events</h2>
-      <event>
-	<img class="cover"
-             src="/events/bhd-poster.jpg"
-             alt="Bright, Hope, Dream">
-	<p> Bright, Hope, Dream: TiA! Academic and Life Abroad Workshop <br/> First Talk: Canada and Nordic Countries @ 2023-10-28 14:00 UTC <br/> Join our <a href="#top">Social Media</a> for details. </p>
-      </event>
-      <event>
-	<img class="cover"
-             src="/events/lacan-reading.png"
-             alt="Lacan reading group">
-	<p> Reading Group: Lacanian Psychoanalysis for Activists <br/> Online from 2023-06-17 to 2023-08-10. </p>
-      </event>
-      <event>
-	<img class="cover"
-             src="/events/deleuze-reading.jpg"
-             alt="Deleuze reading group">
-	<p> Reading Group of <i> A Thousand Plateaus </i> <br/> Online from 2023-04-22. <br/> First series of seminars ended. Second in preparation. </p>
-      </event>
-      <event>
-	<img class="cover"
-             src="/events/miyazawa-poster.jpg"
-             alt="Poster of livestream reading of Night of the Milky Way Railway">
-	<p> Livestream reading of <i>Night of the Milky Way Railway</i> on TDOV 2023, by One Among Us & Project Trans. <br/> <a href="https://www.youtube.com/watch?v=TGhNTd-2I3c"> Replay </a> is available on YouTube. </p>
-      </event>
-    </section>
-  </main>
+  <EventsList/>
   </div>
 </template>
 
 <script>
+import EventsList from './EventsList.vue'
+export default {
+  components: {
+    EventsList
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
+<style lang="scss">
 $margin-width: 16rem !default;
 $body-min-width: 20rem !default;
 $container-max-width: 40rem !default;
@@ -67,12 +36,12 @@ $mobile-breakpoint: $margin-width * 2 + $body-min-width * 1.2 !default;
 }
 // big screens
 @media screen and (min-width: $container-max-width) {
-    div {
+    div .content {
         padding-left: calc(100% - $container-max-width);
 	padding-right: calc(100% - $container-max-width);
     }
 }
-div {
+div .content {
     max-width: calc(min(#{$container-max-width}, 100%));
     padding: $padding-content;
     box-sizing: content-box;
@@ -81,19 +50,13 @@ div {
     margin-block-start: 0;
     margin-block-end: 2rem;
     display: grid;
-
+}
 
 header {
     grid-column-start: 2;
     grid-column-end: 3;
     grid-row: 2;
 }
-main {
-    grid-column-start: 2;
-    grid-column-end: 3;
-    grid-row: 3;
-}
-
 h2 {
     font-size: 2em;
     text-align: center;
@@ -105,6 +68,5 @@ p {
 img {
     width: calc(min(#{$container-max-width}, 100%));
     vertical-align: middle;
-}
 }
 </style>
