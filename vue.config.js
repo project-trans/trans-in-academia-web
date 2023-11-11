e55cf6bb99ac79c4a422d85bf2aca921983a9cd5
sauricat <linshu1729@protonmail.com>
2023-11-11 00:07:56 -0500

    [U] Update title, icon

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
     chainWebpack: config => {
+        config.plugin('html').tap(args => {args[0].title = "Trans in Academia!"; return args; })
         config.module
             .rule('yaml')
             .test(/\.ya?ml$/)
